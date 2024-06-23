88c87c6b4d0fccd9b8388810854e397d04767636
IOSTRM <118191540+IOSTRM@users.noreply.github.com>
2024-06-22 20:41:40 -0400

    Delete util.hs

--- util.hs
+++
@@ -1,8 +0,0 @@
-module Util (powerMod) where
--- Returns (b^e) mod m
-powerMod :: (Integral a, Integral b) => a -> b -> a -> a
-powerMod b e m 
-    | e == 0 = 1
-    | (odd e) = ( b * (powerMod (temp) (e `div` 2) m)) `mod` m 
-    | otherwise = (powerMod (temp) (e `div` 2) m)
-    where temp = (b * b) `mod` m
